583a8bcac802d9a9de17dacb731d306fb7f37193
Jan T <jan-tee@users.noreply.github.com>
2022-10-05 10:53:12 +0200

    Delete deploy-k3s-demo.cmd

--- charts/bind9/deploy-k3s-demo.cmd
+++
@@ -1,5 +0,0 @@
-kubectx k3s-demo
-kubectl create namespace external-dns
-helm upgrade --install --namespace external-dns ^
-  my-dns . ^
-  --set service.annotations."metallb\.universe\.tf/loadBalancerIPs"="10.41.0.107"
