b78384f7c0d06a21076a91c748c1e47a3732704c
Jan T <jan-tee@users.noreply.github.com>
2022-10-05 10:53:04 +0200

    Delete deploy-bart.cmd

--- charts/bind9/deploy-bart.cmd
+++
@@ -1,8 +0,0 @@
-kubectx bart
-kubectl create namespace external-dns
-helm upgrade --install --namespace external-dns ^
-  authoritative-dns . ^
-  --set service.annotations."metallb\.universe\.tf/loadBalancerIPs"="192.168.42.230" ^
-  --set service.annotations."metallb\.universe\.tf/allow-shared-ip"="main-ip" ^
-  --set keys.k8s-external-dns.secret="XFf2PPo0p4HLdkOlnTT4KKQl01apR3BXPaLBhtSY6H8="
-@rem   --dry-run --debug
